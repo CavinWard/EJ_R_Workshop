22095594d0bf36a3262ce84c5d39d1e204a251d6
Cavin Ward-Caviness <cavin.wardcaviness@gmail.com>
2019-01-11 13:48:45 -0500

    outlining workshop

---
+++ R_code/Census SES and Environment v1.R
@@ -0,0 +1,23 @@
+### EJ and Air Quality R workshop script
+
+rm(list=ls())
+
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(tidycensus)
+
+#### load in downloaded PM2.5 data
+
+#### load in brownfields data
+
+#### get census data from tidycensus
+
+### build block group classifiers
+
+### build neighborhoods
+
+### regress bg classifiers and neighborhoods on PM and brownfields data
+
+### expansion exercise with roadway length
+
